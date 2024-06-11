de20ec723be39c6093e56dae6c88687be91b6f5d
nguyenthien_tran <trannguyenthien@mecury.com.vn>
2024-06-11 14:27:46 +0700

    create portfolio page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,19 @@ import {NotFoundPageComponent} from "./not-found-page/not-found-page.component";
 
 export const routes: Routes = [
   {
-    path: "profile", component: ProfilePageComponent
+    path: "profile", component: ProfilePageComponent,
+    data: {
+      title: "Portfolio",
+      values: {
+        url: "localhost",
+        port: "8080"
+      }
+    }
   },
   {
     path: "", redirectTo: "/profile", pathMatch: "full"
   },
   {
-    path: "**", component: NotFoundPageComponent
+    path: "**", component: NotFoundPageComponent, title: "Not Found Page"
   }
 ];
--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, effect, signal} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {CommandModule} from "@angular/cli/src/command-builder/command-module";
+import {data} from "autoprefixer";
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-profile-page',
@@ -8,5 +12,18 @@ import { Component } from '@angular/core';
   styleUrl: './profile-page.component.css'
 })
 export class ProfilePageComponent {
+  readonly  age = signal(0);
+
+  constructor(private activeRoute: ActivatedRoute, private title: Title) {
+    activeRoute.data.subscribe(data => this.title.setTitle(data['title']));
+    effect(() => {
+      console.log(`The current age is: ${this.age()}`);
+    });
+  }
+  increment = () => {
+    this.age.update(value => value + 1);
+
+  }
+
 
 }
