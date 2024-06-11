077cb0cbcea7fd0f2737aa1e1045cc72bcbd5f9b
nguyenthien_tran <trannguyenthien@mecury.com.vn>
2024-06-11 10:52:08 +0700

    init project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {ProfilePageComponent} from "./profile-page/profile-page.component";
+import {NotFoundPageComponent} from "./not-found-page/not-found-page.component";
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: "profile", component: ProfilePageComponent
+  },
+  {
+    path: "", redirectTo: "/profile", pathMatch: "full"
+  },
+  {
+    path: "**", component: NotFoundPageComponent
+  }
+];
